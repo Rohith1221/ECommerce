8fda721c4f0542d58bb422f5e23ac97391021e2e
Rohith <marganirohith2000@gmail.com>
2022-04-30 01:05:12 +0530

    connecting to sanity

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,33 @@
 import React from "react";
 import { Product, FooterBanner, Footer, HeroBanner } from "../components";
-const index = () => {
+import { client } from "../lib/client";
+
+const index = ({ product, bannerData }) => {
   return (
     <>
-      <HeroBanner />
+      {console.log(bannerData)}
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
       <div className="products-heading">
         <h2>Best Selling products</h2>
         <p>Speakers of many variations</p>
       </div>
       <div className="products-container">
-        {["product 1 ", "product 2 ", "product 3 "].map((prod) => prod)}
+        {product?.map((prod) => prod.name)}
       </div>
       <FooterBanner />
     </>
   );
 };
 
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const product = await client.fetch(query);
+
+  const BannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(BannerQuery);
+
+  return {
+    props: { bannerData, product },
+  };
+};
 export default index;
